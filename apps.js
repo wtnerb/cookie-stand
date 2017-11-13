5c01e2a03d91a61bca3892251a7632f9c89c752a
Brent Williams <wtnerb@gmail.com>
2017-11-13 15:11:05 -0800

    added daily method to pike

--- apps.js
+++ apps.js
@@ -6,6 +6,14 @@ var pike = {
   max: 65,
   avg: 6.3,
   hour: function (){
-    return (Math.floor((1 + this.max - this.min) * Math.random() + this.min);
+    return Math.floor((1 + this.max - this.min) * Math.random() + this.min);
+  },
+  day: function (){
+    for (var i = 8; i < 12; i++){
+      document.write(i, 'am', this.hour());
+    }
+    for (var j = 0; j < 9; j++){
+      document.write(j, 'pm', this.hour());
+    }
   }
 };
